77aba78f382128d155cc0c028be9215a6634c895
thantez <bakhshi@protonmail.com>
2019-12-29 20:49:31 +0330

    change heroku configs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :maze_server, MazeServerWeb.Endpoint,
-  url: [scheme: "https", host: "mazeai", port: 443],
+  url: [scheme: "https", host: "mazeai.herokuapp.com", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
